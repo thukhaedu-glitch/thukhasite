8fe4ad09741b1215e0d35c070fd2c76b22f1a523
thukhaedu-glitch <thukha.edu@gmail.com>
2026-06-21 14:15:15 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App";
+import AdminPanel from "./components/AdminPanel";
+import "./index.css";
+
+const isAdminPage =
+  window.location.pathname === "/admin" ||
+  window.location.pathname.startsWith("/admin/");
+
+createRoot(
+  document.getElementById("root")!,
+).render(
   <StrictMode>
-    <App />
+    {isAdminPage ? <AdminPanel /> : <App />}
   </StrictMode>,
 );
